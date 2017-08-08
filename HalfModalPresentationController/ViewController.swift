3e33183e32a4f1b49f80cb2c5308892b6fadff0c
Michael Lema <Mel526@nyu.edu>
2017-08-08 16:21:29 -0400

    Initial Commit

---
+++ HalfModalPresentationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HalfModalPresentationController
+//
+//  Created by Michael Lema on 8/8/17.
+//  Copyright © 2017 Michael Lema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
